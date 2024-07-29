77495b5b0ea91ec7827b7385eabc1fe383b393cf
Aerospike-NateL <nlarsen@aerospike.com>
2024-07-29 12:03:42 -0700

    idk

--- aerospike/src/aerospike/nemesisChk.clj
+++ aerospike/src/aerospike/nemesisChk.clj
@@ -6,7 +6,20 @@
             [jepsen [client :as client]
              [checker :as checker]
              [generator :as gen]])
-  )
+  (:import (clojure.lang ExceptionInfo)
+           (com.aerospike.client AerospikeClient
+                                 AerospikeException
+                                 AerospikeException$Connection
+                                 AerospikeException$Timeout
+                                 Bin
+                                 Info
+                                 Key
+                                 Record)
+           (com.aerospike.client.cluster Node)
+           (com.aerospike.client.policy Policy
+                                          ;; ConsistencyLevel
+                                        GenerationPolicy
+                                        WritePolicy)))
 
 (defrecord myClient [client namespace set]
   client/Client
@@ -21,8 +34,7 @@
   (myClient. nil s/ans "foo"))
 
 (defn workload []
-  {
-   :client (myClient)
-   :checker (checker/set)
-   :generator (dbNemesis/full-gen {})
+  {:client (myClient)
+;;    :checker (checker/set)
+;;    :generator (dbNemesis/full-gen {})
   })
