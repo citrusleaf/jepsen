385efea6b05c709746f17e040738e5547a949919
Aerospike-NateL <nlarsen@aerospike.com>
2024-07-30 16:39:06 -0700

    still debug test.generator

--- aerospike/src/aerospike/core.clj
+++ aerospike/src/aerospike/core.clj
@@ -77,7 +77,7 @@
                                 (gen/clients final-generator)))]
     (info "constructed jepsen test-map!")
     (info "jepsen test total generator's first's first's first>")
-    (info (first (first (first (first generator)))))
+    (info "test.generator's `gen` is a"(type (:gen generator)))
     ;; ;; (info (gen/op (first (first generator))))
     
   (merge tests/noop-test
