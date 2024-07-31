e0d572bde05dd0eb70528089b26e512261359566
Aerospike-NateL <nlarsen@aerospike.com>
2024-07-31 12:17:45 -0700

    retry

--- aerospike/src/aerospike/nemesis.clj
+++ aerospike/src/aerospike/nemesis.clj
@@ -6,7 +6,7 @@
                     [nemesis :as nemesis]
                     [util :refer [meh random-nonempty-subset]]]
             [jepsen.nemesis.time :as nt]
-            [jepsen.generator.pure :refer [once]]))
+            [jepsen.generator.pure :as gen]))
 
 ; Nemeses
 
@@ -99,7 +99,7 @@
   [test]
    (let [disrupts (killer-gen-seq test)]
      (info "(killer-gen-seq) returned: [" (take 5 disrupts) "..]")
-     (once disrupts)
+     (gen/once disrupts)
     )
 )
 
